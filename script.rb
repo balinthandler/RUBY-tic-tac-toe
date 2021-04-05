7e696f91a0db57149b52899d3a93ab5a53157572
Balint Handler <handlerbalint@gmail.com>
2021-04-05 13:29:45 +0200

    Initializing game, get players name and sign, make empty board

--- script.rb
+++ script.rb
@@ -1,5 +1,23 @@
 require 'pry'
+
 # binding.pry at any point un the program to make a breakpoint
+
+
+class Game 
+  def initialize
+    #Get player names and signs
+    puts "Player1"
+    @player1 = Player.new()
+    puts "Player2"
+    @player2 = Player.new()
+    
+    #Initializing empty board
+    @table = Table.new()
+    @table.show_table()
+  
+  end
+end
+
 
 # Multi-dimensional array for table
 # 3
@@ -8,22 +26,44 @@ require 'pry'
 #   A B C
 class Table 
   def initialize
-    @table = [['-','-','-'],['-','-','-'],['-','-','-']]
+    @@table = [['-','-','-'],['-','-','-'],['-','-','-']]
   end
-
-  def input(row,column,sign) 
-    @table[row][column] = sign
-  end
-
   def show_table
-    puts "3 #{@table[0][0]} #{@table[0][1]} #{@table[0][2]}"
-    puts "2 #{@table[1][0]} #{@table[1][1]} #{@table[1][2]}"
-    puts "1 #{@table[2][0]} #{@table[2][1]} #{@table[2][2]}"
+    puts "3 #{@@table[0][0]} #{@@table[0][1]} #{@@table[0][2]}"
+    puts "2 #{@@table[1][0]} #{@@table[1][1]} #{@@table[1][2]}"
+    puts "1 #{@@table[2][0]} #{@@table[2][1]} #{@@table[2][2]}"
     puts "  A B C"
-
+  end
+  def input(column, row, sign)
+    @@table[column][row] = sign
   end
 end
+
+# Player class with instance methods and variables
 # Sign choosing at beginnings
+class Player
+  def initialize
+    puts "What's your name?"
+    @name = gets.chomp
+    puts "Would you be O or X?"
+    @sign = gets.chomp.upcase
+    until @sign == 'O' || @sign == 'X'
+      puts "Please choose between O and X!"
+      @sign = gets.chomp.upcase
+    end
+      puts "Welcome, #{@name}! Your sign is: #{@sign}"
+      puts "-----------------------------------------"
+  end
+  def sign 
+    return @sign
+  end
+  def name
+    return @name
+  end
+end
+
+Game.new()
+
 
 # Input move
 
@@ -33,19 +73,6 @@ end
 
 
 
-# Player class with instance methods and variables
-class Player
-  def initialize(name)
-    @name = name
-  end
 
-  def greeting
-    puts "Welcome, #{@name}"
-  end
-end
 
-player = Player.new('Bálint')
-table = Table.new()
 
-player.greeting()
-table.show_table()
