87d412c07e6555482c6e50bb5e2d994073f51774
Balint Handler <handlerbalint@gmail.com>
2021-04-07 12:40:03 +0200

    removed unused lines

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
-
 class Game 
   
+  #create empty board & get player names and signs
   def initialize
     @players = []
     @table = Board.new()
@@ -30,6 +30,7 @@ class Game
     }
   end
 
+  #checking if entered move is valid
   def input_check(move)
 
     if move.length != 2 
@@ -49,6 +50,8 @@ class Game
       end
     end 
   end
+
+  #playing turns until game over scenario
   def game_mechanism
     @table.show_board
     turn = 1
@@ -77,6 +80,8 @@ class Game
     end
     gameover_reset()
   end
+
+  #reset table
   def gameover_reset
     if @table.game_ended == true
       puts "Want to play another round? Then type: play"
@@ -92,33 +97,38 @@ class Game
 end
 
 class Board 
+  #empty board
   def initialize
 		@board = [['-','-','-'],['-','-','-'],['-','-','-']]
     @game_ended = false
   end
+
+  #returns game is ended or not
   def game_ended
     return @game_ended
   end
+
+  #evaluates board 
   def board_evaluation(players)
     for i in 0..2
       if @board[i].all? { |item| item == @board[i][0] && item != '-'} 
         if @board[i][0] == players[0].sign()
-          show_result(players[0].name)
+          show_winner(players[0].name)
           @game_ended = true
           break
         else  
-          show_result(players[1].name)
+          show_winner(players[1].name)
           @game_ended = true
           break
         end
       elsif @board[0][i] == @board[1][i] && @board[1][i] == @board[2][i] && @board[0][i] != '-' then
         if @board[0][i] == players[0].sign()
           @game_ended = true
-          show_result(players[0].name)
+          show_winner(players[0].name)
           break
         else  
           @game_ended = true
-          show_result(players[1].name)
+          show_winner(players[1].name)
           break
         end
       end
@@ -128,10 +138,10 @@ class Board
         @board[0][2] == @board[1][1] && @board[2][0] == @board[1][1] && @board[2][2] != '-' 
         if @board[1][1] == players[0].sign()
           @game_ended = true
-          show_result(players[0].name)
+          show_winner(players[0].name)
         else
           @game_ended = true
-          show_result(players[1].name)
+          show_winner(players[1].name)
         end
       end
     end
@@ -150,6 +160,7 @@ class Board
     
   end
 
+  #puts board on screen
   def show_board
     puts "3 #{@board[2][0]} #{@board[2][1]} #{@board[2][2]}"
     puts "2 #{@board[1][0]} #{@board[1][1]} #{@board[1][2]}"
@@ -157,16 +168,22 @@ class Board
     puts "  A B C"
   end
 
+  #write player sign on board
   def input(row, column, sign)
       @board[row][column] = sign  
   end
   
+  #return @board
   def board_reading
     return @board
   end
-  def show_result(winner)
+  
+  #show winner
+  def show_winner(winner)
     puts "#{winner} have won the match"
   end
+
+  #reset board and game_ended boolean
 	def reset
 		@board = [['-','-','-'],['-','-','-'],['-','-','-']]
     @game_ended = false
@@ -175,14 +192,15 @@ class Board
 
 end
 
-# Player class with instance methods and variables
-# Sign choosing at beginnings
+
 class Player
+  #assign names and signs
   def initialize(name, sign)
     @name = name
     @sign = sign
   end
 
+  #welcome message for players
   def greeting
     puts "Welcome, #{@name}! Your sign is: #{@sign}"
   end
@@ -197,15 +215,5 @@ end
 
 new_game = Game.new()
 new_game.game_mechanism
-
-# Input move
-
-#checking already taken spots
-
-#win-checker
-
-
-
-
 
 
